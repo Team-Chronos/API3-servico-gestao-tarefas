a4eccef3c55b93d69027cc7b9f9d4155f220138d
Snake Eater <155785922+gabsact4@users.noreply.github.com>
2026-03-20 18:15:00 -0300

    refactor: (#47) Enviar dados para API

--- criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/controle/controleApontamento.java
+++ criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/controle/controleApontamento.java
@@ -1,14 +1,23 @@
 package com.criarTarefas.criarTarefas.controle;
 
-import com.criarTarefas.criarTarefas.modelo.DTO.apontamentoDTO;
-import com.criarTarefas.criarTarefas.modelo.modeloApontamento;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import com.criarTarefas.criarTarefas.servico.servicoApontamento;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.criarTarefas.criarTarefas.modelo.DTO.apontamentoDTO;
+import com.criarTarefas.criarTarefas.modelo.modeloApontamento;
+import com.criarTarefas.criarTarefas.servico.servicoApontamento;
+
 import jakarta.validation.Valid;
-import java.util.List;
 
 @RestController
 
