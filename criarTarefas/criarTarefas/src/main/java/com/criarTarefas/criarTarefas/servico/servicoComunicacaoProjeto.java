a97d3aa92f00055db63b5eb418cc9ef05724a1f2
Snake Eater <155785922+gabsact4@users.noreply.github.com>
2026-04-03 10:45:52 -0300

    refactor(#89):atualizando metodo de comunicação

--- criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/servico/servicoComunicacaoProjeto.java
+++ criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/servico/servicoComunicacaoProjeto.java
@@ -1,11 +1,10 @@
 package com.criarTarefas.criarTarefas.servico;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -83,7 +82,6 @@ public class servicoComunicacaoProjeto {
     
     @GetMapping("/responsaveis")
     public ResponseEntity<?> listarResponsaveisDosProjetos() {
-        System.out.println("=== Listando responsáveis dos projetos ===");
         
         try {
             String url = apiUrl;
