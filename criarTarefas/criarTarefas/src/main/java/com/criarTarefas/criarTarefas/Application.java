292f632fcddfa37c7cc60a72cbabc279cbf2e78a
Lucas Marins <lucasmarins2024@gmail.com>
2026-04-25 21:57:33 -0300

    feat (#106): otimizar tempo de resposta

--- criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/Application.java
+++ criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/Application.java
@@ -2,11 +2,9 @@ package com.criarTarefas.criarTarefas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableCaching
 @ComponentScan(basePackages = {"com.criarTarefas.criarTarefas"})
 public class Application {
 
